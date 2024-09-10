80991df9766bb6c31896e74cd30bf6ca329e8793
Danis <danis11255@gmail.com>
2024-09-10 22:57:20 +0500

    More features for orderPage

--- frontend/src/components/OrderItem.tsx
+++ frontend/src/components/OrderItem.tsx
@@ -8,7 +8,7 @@ interface AdItemProps {
 const OrderItem: React.FC<AdItemProps> = ({order}) => {
     return(
     <div className="content__orders__order">
-        <h3 className="content__orders__order__title">{ad.name}</h3>
+        <h3 className="content__orders__order__title">Test</h3>
         <ul className="content__orders__order__list">
             <li className="content__orders__order__stat"> </li>
             <li className="content__orders__order__stat"> </li>
