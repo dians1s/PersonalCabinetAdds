2f96ce0b1c3e533a6ccb0ea3b1da28ad4447c055
Danis <danis11255@gmail.com>
2024-09-10 22:03:41 +0300

    Fix filter options

--- frontend/src/components/Ads.tsx
+++ frontend/src/components/Ads.tsx
@@ -8,28 +8,28 @@ interface AdsItemProps {
     ads: Advertisment[];
     isAdsLoading: boolean;
     adsError: string;
-    filter: {sort: string, query: string};
-    setFilter: (filter: {sort: string, query: string}) => void;
+    filter: { sort: string, query: string };
+    setFilter: (filter: { sort: string, query: string }) => void;
     sortedAndSearchAds: Advertisment[];
 }
 
-const Ads: React.FC<AdsItemProps> = ({ads, isAdsLoading, adsError, filter, setFilter, sortedAndSearchAds}) => {
+const Ads: React.FC<AdsItemProps> = ({ ads, isAdsLoading, adsError, filter, setFilter, sortedAndSearchAds }) => {
 
-    return(
+    return (
         <div className="content">
             <h2 className="visually-hidden">Мои объявления</h2>
             <FilterAndSearch filter={filter} setFilter={setFilter}>
                 <option value="id">Без сортировки</option>
-                <option value="price">По сумме заказа</option>
+                <option value="price">По цене</option>
                 <option value="views">Просмотрам</option>
                 <option value="likes">Лайкам</option>
             </FilterAndSearch>
             {isAdsLoading
-                ? <div style={{width: '100%', minHeight: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><Loader /></div>
-                : <AdList ads={sortedAndSearchAds} adsError={adsError}/>
+                ? <div style={{ width: '100%', minHeight: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}><Loader /></div>
+                : <AdList ads={sortedAndSearchAds} adsError={adsError} />
             }
-            {adsError && <h3 style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '36px', textAlign: 'center', width: '100%', color: '#da0d0d'}}>Произошла ошибка при загрузке данных</h3>}
-            
+            {adsError && <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '36px', textAlign: 'center', width: '100%', color: '#da0d0d' }}>Произошла ошибка при загрузке данных</h3>}
+
         </div>
     )
 }
--- frontend/src/components/Orders.tsx
+++ frontend/src/components/Orders.tsx
@@ -5,26 +5,26 @@ import FilterAndSearch from "./FilterAndSearch";
 import { Order } from "../types/types";
 
 interface OrdersProps {
-    filter: {sort: string, query: string};
-    setFilter: (filter: {sort: string, query: string}) => void;
+    filter: { sort: string, query: string };
+    setFilter: (filter: { sort: string, query: string }) => void;
     isOrdersLoading: boolean;
     orders: Order[];
     ordersError: string;
 }
 
-const Orders: React.FC<OrdersProps> = ({filter, setFilter, isOrdersLoading, orders, ordersError}) => {
-    return(
+const Orders: React.FC<OrdersProps> = ({ filter, setFilter, isOrdersLoading, orders, ordersError }) => {
+    return (
         <div className="content">
             <h2 className="visually-hidden">Мои заказы</h2>
             <FilterAndSearch filter={filter} setFilter={setFilter}>
-
+                <option value="id">Без сортировки</option>
+                <option value="price">По сумме заказа</option>
             </FilterAndSearch>
             {isOrdersLoading
-                ? <div style={{width: '100%', minHeight: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}><Loader /></div>
-                : <OrdersList orders={orders} ordersError={ordersError}/>
+                ? <div style={{ width: '100%', minHeight: '50vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}><Loader /></div>
+                : <OrdersList orders={orders} ordersError={ordersError} />
             }
-            {ordersError && <h3 style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '36px', textAlign: 'center', width: '100%', color: '#da0d0d'}}>Произошла ошибка при загрузке данных</h3>}
-            
+            {ordersError && <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '36px', textAlign: 'center', width: '100%', color: '#da0d0d' }}>Произошла ошибка при загрузке данных</h3>}
         </div>
     )
 }
